db55ca2f38e692a3d27f1d0cd3fdbf89b4e07c21
Gonçalo Marques <goncalo.an.marques@mediamonks.com>
2023-07-19 16:12:45 +0200

    wip

--- 07-cameras/src/script.js
+++ 07-cameras/src/script.js
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
 //cursor
 const cursor = {
@@ -7,8 +8,8 @@ const cursor = {
 };
 
 window.addEventListener("mousemove", (event) => {
-  cursor.x = event.clientX / sizes.width;
-  console.log(cursor.x);
+  cursor.x = event.clientX / sizes.width - 0.5;
+  cursor.y = -(event.clientY / sizes.height - 0.5);
 });
 
 /**
@@ -52,9 +53,15 @@ const camera = new THREE.PerspectiveCamera(
 // );
 // camera.position.x = 2;
 // camera.position.y = 2;
+
 camera.position.z = 2;
 camera.lookAt(mesh.position);
 scene.add(camera);
+
+//controls
+const controls = new OrbitControls(camera, canvas);
+// controls.target.y = 2;
+// controls.update();
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
@@ -70,6 +77,12 @@ const tick = () => {
 
   // Update objects
   // mesh.rotation.y = elapsedTime;
+
+  //Update
+  camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3;
+  camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3;
+  camera.position.y = cursor.y * 5;
+  camera.lookAt(mesh.position);
 
   // Render
   renderer.render(scene, camera);
