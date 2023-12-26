31c23858c041d2adfaebe4261538688da4d7636f
floppydisk89 <53581002+floppydisk89@users.noreply.github.com>
2023-12-26 01:12:48 +0100

    images

--- customizer/src/scene.tsx
+++ customizer/src/scene.tsx
@@ -50,7 +50,7 @@ function Shirt() {
   const texture = useTexture(`/${snap.selectedDecal}.png`);
   const { nodes, materials } = useGLTF("/shirt_baked_collapsed.glb");
 
-  useFrame((state, delta) => {
+  useFrame((_frameState, delta) => {
     easing.dampC(
       (materials.lambert1 as MeshLambertMaterial).color,
       snap.selectedColor,
@@ -73,7 +73,7 @@ function Shirt() {
       <Decal
         position={[0, 0.04, 0.15]}
         rotation={[0, 0, 0]}
-        scale={0.15}
+        scale={0.2}
         map={texture}
       />
     </mesh>
--- customizer/src/store.ts
+++ customizer/src/store.ts
@@ -11,9 +11,9 @@ type RootState = {
 const state = proxy<RootState>({
   intro: true,
   colors: ["#ccc", "#EFBD4E", "#80C670", "#726DE8", "#EF674E", "#353934"],
-  decals: ["react", "three2", "pmndrs"],
+  decals: ["dogecoin", "ethereum"],
   selectedColor: "#EFBD4E",
-  selectedDecal: "three2",
+  selectedDecal: "dogecoin",
 });
 
 export { state };
