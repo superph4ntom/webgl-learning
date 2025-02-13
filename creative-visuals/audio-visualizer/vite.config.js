be3bfe9833a3d920815d40be10e9fcb230ad0310
floppydisk89 <53581002+floppydisk89@users.noreply.github.com>
2025-02-13 19:12:22 +0100

    fix - remove obfuscator

--- creative-visuals/audio-visualizer/vite.config.js
+++ creative-visuals/audio-visualizer/vite.config.js
@@ -1,26 +1,9 @@
 import { defineConfig } from "vite";
-import obfuscatorPlugin from "vite-plugin-javascript-obfuscator";
 
 export default defineConfig(async () => {
   const config = {
-    plugins: [
-      obfuscatorPlugin({
-        include: ["src/**/*.js"], // Adjust to match your file structure
-        exclude: ["/node_modules"],
-        options: {
-          compact: true,
-          controlFlowFlattening: true,
-          deadCodeInjection: true,
-          stringArray: true,
-          stringArrayThreshold: 0.75,
-          rotateStringArray: true,
-          selfDefending: true,
-          debugProtection: true,
-        },
-      }),
-    ],
+    plugins: [],
     build: {
-      minify: "terser", // Ensures code is minified after obfuscation
       target: "esnext",
       sourcemap: false, // Disable sourcemaps for better protection
     },
