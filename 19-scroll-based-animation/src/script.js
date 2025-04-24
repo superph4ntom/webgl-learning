13b3bd9d0f91ef2c7b7e22bc717f9573192b8b26
Gonçalo Marques <146715510+K0ndr4kula@users.noreply.github.com>
2025-04-24 16:38:41 +0200

    19 - wip

--- 19-scroll-based-animation/src/script.js
+++ 19-scroll-based-animation/src/script.js
@@ -7,13 +7,15 @@ import * as THREE from "three";
 const gui = new GUI();
 
 const parameters = {
-  materialColor: "#ffeded",
+  materialColor: "#ffdeded",
+  objectsDistance: 4,
 };
 
 gui.addColor(parameters, "materialColor");
 gui.addColor(parameters, "materialColor").onChange(() => {
   material.color.set(parameters.materialColor);
 });
+
 /**
  * Base
  */
@@ -23,23 +25,41 @@ const canvas = document.querySelector("canvas.webgl");
 // Scene
 const scene = new THREE.Scene();
 
+// Texture
+const textureLoader = new THREE.TextureLoader();
+const gradientTexture = textureLoader.load("textures/gradients/3.jpg");
+// interpolate
+gradientTexture.magFilter = THREE.NearestFilter;
+
 // Material
 const material = new THREE.MeshToonMaterial({
   color: parameters.materialColor,
   gradientMap: gradientTexture,
 });
 
-// Objects
+// Meshes
 const mesh1 = new THREE.Mesh(new THREE.TorusGeometry(1, 0.4, 16, 60), material);
-
-const mesh2 = new THREE.Mesh(new THREE.ConeGeometry(1, 0.4, 16, 60), material);
-
+const mesh2 = new THREE.Mesh(new THREE.ConeGeometry(1, 2, 32), material);
 const mesh3 = new THREE.Mesh(
   new THREE.TorusKnotGeometry(0.8, 0.35, 100, 16),
   material
 );
 
+// mesh1.position.y = 2;
+// mesh1.scale.set(0.5, 0.5, 0.5);
+
+// mesh2.visible = false;
+
+// mesh3.position.y = -2;
+// mesh3.scale.set(0.5, 0.5, 0.5);
+
+mesh1.position.y = -parameters.objectsDistance * 0;
+mesh2.position.y = -parameters.objectsDistance * 1;
+mesh3.position.y = -parameters.objectsDistance * 2;
+
 scene.add(mesh1, mesh2, mesh3);
+
+const sectionMeshes = [mesh1, mesh2, mesh3];
 
 /**
  * Lights
@@ -110,6 +130,11 @@ const clock = new THREE.Clock();
 
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
+
+  for (const mesh of sectionMeshes) {
+    mesh.rotation.x = elapsedTime * 0.1;
+    mesh.rotation.y = elapsedTime * 0.12;
+  }
 
   // Render
   renderer.render(scene, camera);
